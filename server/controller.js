732c9846c153805579675295a4f254c26f034011
Lisa Nam <lsyn2016@gmail.com>
2017-01-02 12:07:48 -0800

    Set up basic react router

--- server/controller.js
+++ server/controller.js
@@ -5,7 +5,40 @@ var appKey = '1f821247a9df4a255feed4e2895fec36';
 
 
 module.exports = {
-  addNew: function (req, res) {
+  add: function (req, res) {
+    var gallery = req.body.patient;
+    var subject = req.body.subject;
+    var photo = req.body.photo;
+
+    var request = new XMLHttpRequest();
+    request.open('POST', baseURL + '/enroll');
+
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.setRequestHeader('app_id', appId);
+    request.setRequestHeader('app_key', appKey);
+
+    request.onreadystatechange = function () {
+      if (this.readyState === 4) {
+        console.log('Status:', this.status);
+        console.log('Headers:', this.getAllResponseHeaders());
+        console.log('Body:', this.responseText);
+      }
+    };
+
+    var data = {
+      'gallery_name': gallery,
+      'subject_id': subject,
+      'image': photo
+    };
+
+    request.send(JSON.stringify(data));
+    res.end();
+  },
+
+  find: function (req, res) {
+    var gallery = req.body.patient;
+    var photo = req.body.photo;
+
     var request = new XMLHttpRequest();
 
     request.open('POST', 'https://api.kairos.com/recognize');
@@ -23,36 +56,13 @@ module.exports = {
     };
 
     var data = {
-      'image': 'http://www.bet.com/topics/b/barack-obama/_jcr_content/image.heroimage.dimg/__1358446726593/111412-politics-barack-obama-press-conference.jpg',
-      'gallery_name': 'TestGallery',
+      'image': photo,
+      'gallery_name': gallery,
+      'max_num_results': 3,
       'threshold': '0.63'
     };
 
     request.send(JSON.stringify(data));
-  },
-
-  find: function (req, res) {
-    var request = new XMLHttpRequest();
-    request.open('POST', baseURL + '/enroll');
-
-    request.setRequestHeader('Content-Type', 'application/json');
-    request.setRequestHeader('app_id', appId);
-    request.setRequestHeader('app_key', appKey);
-
-    request.onreadystatechange = function () {
-      if (this.readyState === 4) {
-        console.log('Status:', this.status);
-        console.log('Headers:', this.getAllResponseHeaders());
-        console.log('Body:', this.responseText);
-      }
-    };
-
-    var data = {
-      'image': 'http://media1.s-nbcnews.com/j/newscms/2014_18/418791/140504-obama-correspondents-dinner-750a_53ba5a0e11b559d8a78cdb2962b30bfd.nbcnews-ux-2880-1000.jpg',
-      'subject_id': 'Barack Obama',
-      'gallery_name': 'TestGallery'
-    };
-
-    request.send(JSON.stringify(data));
+    res.end();
   }
 }
