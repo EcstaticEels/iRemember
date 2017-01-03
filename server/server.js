a56861ef4317f1d275a826465eb915be47e4ad35
Jennifer Kao <jennkao92@gmail.com>
2017-01-03 13:50:38 -0800

    test webpack

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 //Basic server
 const express = require('express');
 const app = express();
-const path = require('path');
+var path = require('path');
 
 //Database
 const db = require('../database/db.js');
@@ -12,22 +12,20 @@ const morgan = require('morgan');
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
 app.use(morgan('dev'));
-require('dotenv').config(); //retrieves api keys
+// require('dotenv').config(); //retrieves api keys
 
 //Express static
 app.use(express.static(path.join(__dirname, '..', 'public')));
+// app.use(express.static('/bundle', path.join(__dirname, 'public/bundle')))
 
 //Controllers
 const webControllers = require('./webControllers.js');
 const mobileControllers = require('./mobileControllers.js');
 
-//Routes
-
-app.use(express.static(path.join(__dirname, '..', 'public')));
-
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, '..', 'public/webIndex.html'))
 });
+
 //Web
 app.post('/web/identify', webControllers.addFace);
 app.get('/web/identify', webControllers.retrieveFaces);
