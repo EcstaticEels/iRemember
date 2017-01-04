7de3c134d50030454d5f38c6fa8cc5a6ad29f3dd
Lisa Nam <lsyn2016@gmail.com>
2017-01-03 19:10:35 -0800

    Implement webGallery

--- web/webFaceCurrent.js
+++ web/webFaceCurrent.js
@@ -1,28 +1,14 @@
 import React from 'react';
-import LightboxComponent from 'react-lightbox-component';
-
-var Lightbox = LightboxComponent.Lightbox
+import Gallery from './webGallery.js';
 
 var FaceCurrent = (props) => {
   var edit = () => {
     props.edit(props.current);
   }
 
-  var images =[
-    {
-      src: 'http://mysnoringsolutions.info/wp-content/uploads/2011/12/anti-snoring-pills.jpg',
-      title: 'image title',
-      description: 'image description'
-    }
-  ]
-  // { title: 'image title', description: 'image description', src: 'http://kingofwallpapers.com/pills/pills-011.jpg' }]};
-
   return (
     <div className="face-current">
-      <Lightbox
-        images={images}
-      />
-      <img src={props.current.photos[0]} height="200" width="200"/>
+      <Gallery photos={props.current.photos}/>
       <div>{props.current.subjectName}</div>
       <div>{props.current.description}</div>
       <button onClick={edit}>Edit</button>
