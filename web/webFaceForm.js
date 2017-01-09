8d373a63d85edb50a003dbcbdae058dfe5a868fa
Jennifer Kao <jennkao92@gmail.com>
2017-01-08 17:44:11 -0800

    Fix bugs with face current, allow faces to have audio

--- web/webFaceForm.js
+++ web/webFaceForm.js
@@ -1,34 +1,66 @@
 import React from 'react';
-
+import {Button, Row, Col, Grid} from 'react-bootstrap';
 import ImagesUpload from './webImagesUpload.js';
+import AudioUpload from './webAudioUpload.js';
 
 var FaceForm = (props) => {
-  console.log(props.photos);
+  var uploadedPhotos = props.editMode ?       
+    (<label>Current Uploaded Photos:
+      <div>
+        {props.photos.length > 0 ? props.photos.map((photoObj, ind) => <img src={photoObj.photo} key={ind} height="100" width="130"/>) : null}
+      </div>
+      <br />
+    </label>) : null;
+
+  var uploadedAudio = props.editMode ? 
+    (<label>Current Uploaded Audio Reminder:
+      <br />
+      <audio src={props.audio} controls></audio>
+      <br />
+    </label>) : null;
+
   return (
-    <div className="face-form">
-      <h5>New Face</h5>
-      <form>
-      <label>
-        Name:
-        <input type="text" value={props.subjectName} className="subjectName" placeholder="Name" onChange={props.getInput}/>
-      </label>
-      <br/>
-      <label>Picture:
-        <ImagesUpload getPhotos={props.getPhotos}/>
-      </label>
-      <label>Uploaded Photos:
-        <div>
-          {props.photos.map((photoObj, ind) => <img src={photoObj.photo} key={ind} height="100" width="130"/>)}
-        </div>
-      </label>
-      <br/>
-      <label>Description:</label>
-      <br/>
-        <input type="text" value={props.description} className="description" placeholder="Description" onChange={props.getInput}/>
-      <br/>
-        <input type="submit" value="Submit" onClick={props.submitForm} />
-      </form>
+    <Grid>
+      <div className="face-form">
+        <Row className="show-grid">
+          <h5>New Face</h5>
+        </Row>
+        <form>
+        <Row className="show-grid">
+          <label>
+            Name:
+            <input type="text" value={props.subjectName} className="subjectName" placeholder="Name" onChange={props.getInput}/>
+            <br/>
+          </label>
+        </Row>
+        <Row className="show-grid">
+          <label>Image:
+            <ImagesUpload getPhotos={props.getPhotos}/>
+            <br />
+          </label>
+        </Row>
+        <Row className="show-grid">
+          {uploadedPhotos}
+        </Row>
+        <Row className="show-grid">
+          <label>Upload Audio Message:
+            <AudioUpload getAudio={props.getAudio} />
+            <br/>
+          </label>
+        </Row>
+        <Row className="show-grid">
+          {uploadedAudio}
+        </Row>
+        <Row className="show-grid">
+          <label>Description:
+            <input type="text" value={props.description} className="description" placeholder="Description" onChange={props.getInput}/>
+            <br/>
+          </label>
+        </Row>
+          <input type="submit" value="Submit" onClick={props.submitForm} />
+        </form>
     </div>
+  </Grid>
   );
 }
 
--- web/webFaceListEntry.js
+++ web/webFaceListEntry.js
@@ -2,17 +2,19 @@ import React from 'react';
 import {Grid, Row, Col} from 'react-bootstrap';
 
 var FaceEntry = (props) => {
-  
+  console.log(props.data.photos)
   var changeCurrent = () => {
     props.updateCurrent(props.data);
-  }
+  };
+
+  var photoView = props.data.photos.length > 0 ? <img src={props.data.photos[0].photo} height="100" width="100"/> : null;
 
   return (
     <Grid>
       <Row className="show-grid">
         <div className="face-entry" onClick={changeCurrent}>
           <Col xs={12} md={4}>
-            <img src={props.data.photos[0].photo} height="100" width="100"/>
+            {photoView}
           </Col>
           <Col xs={12} md={8}>
             <h3>{props.data.subjectName}</h3>
