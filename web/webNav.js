489a986a00e299afcefb649c9de295fcdaa779f9
Jennifer Kao <jennkao92@gmail.com>
2017-01-12 13:21:11 -0800

    protect front end and back end routes

--- web/webNav.js
+++ web/webNav.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { Nav, Navbar, NavItem, Button, Grid, Row, Col, NavDropdown, MenuItem } from 'react-bootstrap';
 import { IndexLink } from 'react-router';
 import { LinkContainer, IndexLinkContainer } from 'react-router-bootstrap';
+import Auth from './webAuth.js';
 
 var WebNav = (props) => {
   var onFilterClick = (e) => {
     props.handleStateChange(e);
   };
+
+  var loggedInMsg = Auth.loggedIn() ? <NavItem>Logged in as: {props.name}</NavItem> : null;
   return (
     <Navbar>
       <Navbar.Header>
@@ -15,6 +18,7 @@ var WebNav = (props) => {
         </Navbar.Brand>
       </Navbar.Header>
       <Nav pullRight>
+        {loggedInMsg}
         <NavItem href="/signin">Sign In</NavItem>
         <NavItem href="/signout">Sign Out</NavItem>
       </Nav>
--- web/webSignin.js
+++ web/webSignin.js
@@ -1,25 +1,33 @@
 import React from 'react';
+import $ from 'jquery';
+import Auth from './webAuth.js';
+import { browserHistory } from 'react-router'
 
 export default class Signin extends React.Component {
   constructor(props) {
     super(props);
   }
 
-
-  getInput(event) {
-    var key = event.target.getAttribute('class');
-    var value = event.target.value;
-    var obj = {};
-    obj[key] = value;
-    this.setState(obj);
+  componentDidMount() {
+    this.props.handleLogin(() => {
+      if (Auth.loggedIn() && Auth.needSetup()) {
+        this.props.handleLoginRedirect('/setup');
+      } else if (Auth.loggedIn()) {
+        this.props.handleLoginRedirect('/')
+      }
+    }); 
   }
   
   render() {
     return (
       <div>
         <h1>Sign In</h1>
-        <a href="/auth/google">Sign In with Google</a>
+        <a href='auth/google'>Sign In with Google</a>
       </div>
     )
   }
+}
+
+Signin.contextTypes = {
+  router: React.PropTypes.object.isRequired
 }
