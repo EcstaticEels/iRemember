4b29147b6a651fa1d3b7fa6ce9fcb916a2f979c7
Lisa Nam <lsyn2016@gmail.com>
2017-01-19 12:14:13 -0800

    Implement dropzone

--- web/webImagesUpload.js
+++ web/webImagesUpload.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import {ControlLabel} from 'react-bootstrap';
+import Dropzone from 'react-dropzone'
 
 var ImagesUpload = (props) => (
   <div className="image-upload" name="file" encType="multipart/form-data">
+    <ControlLabel> {'Image:'} </ControlLabel>
+    <Dropzone className='dropzone' onDrop={props.getPhotos} encType="multipart/form-data" multiple>
+      <h3 className='dropzone-text'>Drop files here or click to upload.</h3>
+    </Dropzone>
     <input type="file" className="img" onChange={props.getPhotos}  multiple/>
     <label className='photos-num' htmlFor="img">{props.numFiles} files uploaded</label>
   </div>
