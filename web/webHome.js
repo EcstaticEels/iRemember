64439ffdd0bf4df2205c091415ecbe14304ca5e9
Jennifer Kao <jenniferkao@Jennifers-MacBook-Pro.local>
2017-01-19 22:35:17 -0800

    Add home styling

---
+++ web/webHome.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {Button} from 'react-bootstrap';
+
+export default class Home extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+    <div className="home-container-secondary">
+      <div className="home-container">
+        <div className="home-box">
+          <h2 className="home-description">iRemember is an application designed to help caregivers support patients 
+          struggling with Alzheimer's Disease and other forms of dementia. 
+          <br />
+          <br />
+          iRemember gives caregivers the ability to set reminders for their patients and provides an interface 
+          to help patients recognize important people in their lives using their mobile devices.</h2>
+          <Button bsSize='large' className="get-started-now"><a href="/auth/google" className="get-started-now-text">Sign in to get started now!</a></Button>
+        </div>
+      </div>
+    </div>
+    )    
+  }
+}
