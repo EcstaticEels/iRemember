321cad5a851e936a0888b37f9169235eafcd1016
Jennifer Kao <jennkao92@gmail.com>
2017-01-19 18:59:55 -0800

    Add image preview styling

--- web/webReminder.js
+++ web/webReminder.js
@@ -21,7 +21,7 @@ class Reminder extends React.Component {
       showForm: false,
       editMode: false,
       date: Moment().format(),
-      type: 'medication',
+      type: 'Medication',
       recurring: false,
       selectedDays: {
         Monday: false,
